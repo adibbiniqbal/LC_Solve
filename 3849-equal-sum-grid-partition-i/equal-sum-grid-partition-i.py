ef613c3b56c1661f273e1d67d522f204fdcdbcb4
Adib Bin Iqbal <154238219+adibbiniqbal@users.noreply.github.com>
2026-03-25 15:10:12 +0600

    Time: 168 ms (48.63%) | Memory: 41.3 MB (93.84%) - LeetSync

---
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        total = sum(sum(row) for row in grid)
+        curr = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                curr += grid[r][c]
+            if curr == total - curr:
+                return True
+        curr = 0
+        for c in range(len(grid[0])):
+            for r in range(len(grid)):
+                curr += grid[r][c]
+            if curr == total - curr:
+                return True
+        return False
+        
+        
